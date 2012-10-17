4a752e1ed07e70412863a2af8c96988438636408
Rob <rob@rwstephenson.com>
2012-10-16 22:18:11 -0700

    Yep

---
+++ pages/navbar.php
@@ -0,0 +1,17 @@
+<head><title>Personal Playlist</title>
+</head>
+    <table align='center'><tr>
+	<td><a class='navbar' href='results'>Results</a></td>
+	<td><a class='navbar' href='monthly'>Monthly</a></td>
+	<td><a class='navbar' href='about'>About The Project</a></td>	
+    </tr> </table>
+    <tr>
+        <td><p class='Title'>Rob's Personal Playlist</p></td>
+    </tr>
+<style>
+.Title {font-family:arial; font-size:20px; text-align:center}
+.header {background-color:#CCBB99;  font-family:arial;
+  font-weight:normal; font-size:12px; text-align:center; color:#502020 }
+.body {background-color:#E8E0BB;  font-family:arial;
+  font-weight:normal; font-size:12px; text-align:center; color:#602400 }
+</style>
